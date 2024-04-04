303cf29de6f7d26540e8e0726eeda4d2d0d51250
Vishwajit Avarsekar <vishsecures@gmail.com>
2024-04-04 15:51:34 +0530

    MIddleware and function creation for handling errors

---
+++ api/utils/error.js
@@ -0,0 +1,8 @@
+// creating errors artificially
+
+export const errorHandler = (statusCode, message) => {
+    const error = new Error();
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+};
