f69bca220deb402f4519307e8dbe927680073441
Vishwajit Avarsekar <vishsecures@gmail.com>
2024-04-04 17:27:57 +0530

    signup ui 🔥

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,26 +4,26 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className="bg-green-500" >
+    <header className="bg-[#284d92] shadow-lg" >
 
-        <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
+        <div className="flex justify-between items-center max-w-9xl mx-auto p-2">
         <Link to="/">
-        <h1 className='font-bold text-sm md:text-xl flex flex-wrap'>
-            <span className="text-gray-900 font-serif ">Business</span>
-            <span className="text-gray-700 font-serif ">Name</span>
+        <h1 className=' font-bold text-sm md:text-xl flex flex-wrap'>
+            <span className="text-gray-300 font-serif ">Business</span>
+            <span className="text-gray-300 font-serif ">Name</span>
         </h1>
         </Link>
 
-        <form className="bg-slate-200 p-3 rounded-lg flex items-center">
-            <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
+        <form className="bg-[#84caf0] p-3 rounded-lg flex items-center">
+            <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-80 " />
             <FaSearch className="text-slate-800"/>
         </form>
-        <ul className="flex gap-4">
+        <ul className="flex gap-7">
             <Link to="/about">
-            <li className="text-sans text-black font-bold">About</li>
+            <li className="text-sans text-white font-bold">About</li>
             </Link>
             <Link to ="/sign-in">
-            <li className="text-sans text-black font-bold">Sign in</li>
+            <li className="text-sans text-white font-bold">Sign in</li>
             </Link>
         </ul>
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto ' >
+      <h1 className="font-semibold text-4xl text-center my-16 font-sans">Sign up</h1>
+      <form className='gap-7 flex flex-col' >
+        <input type="text" placeholder="username" className='border p-3 rounded-lg focus:outline-none' id='username' />
+        <input type="text" placeholder="email" className='border p-3 rounded-lg focus:outline-none' id='email' />
+        <input type="text" placeholder="password" className='border p-3 rounded-lg focus:outline-none' id='password' />
+        <button className='bg-blue-900 hover:opacity-80 text-white font-semibold p-3 rounded-lg' >sign up</button>
+      </form>
+      <div className='flex gap-2 mt-2'>
+        <p className='font-semibold' >Already a user?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-[#0e202b] font-sans hover:text-white hover:underline' >sign in</span>
+        </Link>
+      </div>
+     
+    </div>
   )
 }
