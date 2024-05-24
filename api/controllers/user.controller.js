288ce8eb33a3b0a675ad6ecd495648d36f040ece
Vishwajit Avarsekar <vishsecures@gmail.com>
2024-05-24 13:19:37 +0530

    get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,6 +2,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
 import User from "../models/user.model.js";
+import Listing from '../models/listing.model.js'
 
 export const test = (req,res) => {
     res.send("Test API route is working");
@@ -43,6 +44,22 @@ export const deleteUser = async (req, res, next) => {
     } catch (error) {
         next(error)
         
+    }
+
+}
+
+export const getUserListing = async (req, res, next) => {
+   
+    if(req.user.id === req.params.id){
+        try {
+            const listings = await Listing.find({ userRef: req.params.id })
+            res.status(200).json(listings);          
+        } catch (error) {
+            next(error);
+        }
+
+    }else {
+        return next(errorHandler(401, 'Unauthorised to view listings!'))
     }
 
 }
