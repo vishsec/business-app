4f917a0af57b061c07c34735e2d5f51081fc2429
vishsec <vishsecures@gmail.com>
2024-10-09 22:23:19 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use("/api/listing", listingRouter);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.length('*' , (req, res) => {
+app.use('*' , (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
